8aefae21cbd08d0fde15606eeae2a2844930d10e
Zulko <valentin.zulkower@gmail.com>
2018-04-10 18:08:26 +0100

    One more test for AvoidNonuniqueSegments

--- tests/builtin_specifications/test_AvoidNonuniqueSegments.py
+++ tests/builtin_specifications/test_AvoidNonuniqueSegments.py
@@ -3,8 +3,17 @@ from dnachisel import (DnaOptimizationProblem, random_dna_sequence,
 import numpy
 
 # Note: we are not providing a location for AvoidChanges: it applies globally
+def test_AvoidNonuniqueSegments_as_constraint():
+    numpy.random.seed(123)
+    sequence = random_dna_sequence(1000, seed=123)
+    problem = DnaOptimizationProblem(
+        sequence=sequence,
+        constraints=[AvoidNonuniqueSegments(8, location=(0, 1000))])
+    assert not problem.all_constraints_pass()
+    problem.resolve_constraints()
+    assert problem.all_constraints_pass()
 
-def test_AvoidNonuniqueSegments():
+def test_AvoidNonuniqueSegments_as_objective():
     numpy.random.seed(123)
     sequence = random_dna_sequence(1000, seed=123)
     specification = AvoidNonuniqueSegments(8, location=(0, 1000))
