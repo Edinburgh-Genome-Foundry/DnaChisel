07a064c7976c1defe1ab7a329899175a6b9cec17
Zulko <valentin.zulkower@gmail.com>
2019-02-12 19:00:32 +0000

    tests for SequenceLengthBound

---
+++ tests/builtin_specifications/test_SequenceLengthBounds.py
@@ -0,0 +1,9 @@
+import dnachisel as dc
+
+def test_SequenceLengthBounds():
+    for length, expected in [(750, True), (400, False), (1200, False)]:
+        problem = dc.DnaOptimizationProblem(
+            sequence=dc.random_dna_sequence(length),
+            constraints = [dc.SequenceLengthBounds(500, 800)]
+        )
+        assert problem.all_constraints_pass() == expected
