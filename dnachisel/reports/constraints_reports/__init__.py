159abe4aa5a49f5c806ef77a681b7540376fe254
Zulko <valentin.zulkower@gmail.com>
2020-02-04 14:08:30 +0000

    minor change to import GraphicTranslator more easily

--- dnachisel/reports/constraints_reports/__init__.py
+++ dnachisel/reports/constraints_reports/__init__.py
@@ -4,10 +4,11 @@ from .constraints_reports import (
     breaches_records_to_pdf,
     records_from_breaches_dataframe,
 )
-
+from .GraphicTranslator import GraphicTranslator
 __all__ = [
     "records_from_breaches_dataframe",
     "plot_breaches_record",
     "breaches_records_to_pdf",
     "constraints_breaches_dataframe",
+    "GraphicTranslator"
 ]
